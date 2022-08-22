f16caa9134e8a189a264b5c18892bc23817acabc
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-22 20:50:23 +0530

    Initial Commit

---
+++ src/main/java/com/productshut/app/ProductshutApplication.java
@@ -0,0 +1,13 @@
+package com.productshut.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductshutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductshutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productshut/app/ProductshutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productshut.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductshutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
